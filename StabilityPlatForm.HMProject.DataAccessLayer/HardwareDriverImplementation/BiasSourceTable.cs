cae67d99c58604758f68ea6a2bacdda2f78bdf95
燊 杨 <2914269787@qq.com>
2026-03-20 15:48:53 +0800

    添加项目文件。

---
+++ StabilityPlatForm.HMProject.DataAccessLayer/HardwareDriverImplementation/BiasSourceTable.cs
@@ -0,0 +1,14 @@
+﻿using StabilityPlatForm.HMProject.Models.Hardwcare;
+using StabilityPlatForm.HMProject.Models.Interfaces;
+using StabilityPlatForm.HMProject.Utility;
+
+namespace StabilityPlatForm.HMProject.DataAccessLayer.HardwareDriverImplementation
+{
+    public class BiasSourceTable : IBiasSourceTable
+    {
+        public MethodResult<bool> Close() => MethodResult<bool>.Success(true);
+        public MethodResult<bool> Start() => MethodResult<bool>.Success(true);
+        public MethodResult<bool> StopTest() => MethodResult<bool>.Success(true);
+        public MethodResult<bool> TestMode_Vmpp(BiasInfo biasInfo) => MethodResult<bool>.Success(true);
+    }
+}
---
+++ StabilityPlatForm.HMProject.Models/Interfaces/IChannelSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using StabilityPlatForm.HMProject.Models.Hardwcare;
+using StabilityPlatForm.HMProject.Utility;
+
+namespace StabilityPlatForm.HMProject.Models.Interfaces
+{
+    public interface IChannelSwitcher
+    {
+        /// <summary>
+        /// 获取当前通道号
+        /// </summary>
+        public ChannelInfo CurrentChannel { get; }
+        /// <summary>
+        /// 建立连接，启动切换器
+        /// </summary>
+        /// <returns></returns>
+        public MethodResult<bool> Start();
+        /// <summary>
+        /// 断开连接，清理缓存
+        /// </summary>
+        /// <returns></returns>
+        public MethodResult<bool> Close();
+        /// <summary>
+        /// 切换为指定通道
+        /// </summary>
+        /// <returns></returns>
+        public MethodResult<bool> ChannelSwitch(ChannelInfo channelInfo);
+        /// <summary>
+        /// 获取当前通道信息
+        /// </summary>
+        /// <returns></returns>
+        public MethodResult<ChannelInfo> GetChannelAsync();
+    }
+}
